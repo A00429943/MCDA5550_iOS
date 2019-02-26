8302540886c62329671da661449fbace960abf93
Simon Achkar <alachkarsimon@gmail.com>
2019-02-26 10:02:04 -0400

    Updated MapViewController

--- GetGoingClass/ViewControllers/MapViewController.swift
+++ GetGoingClass/ViewControllers/MapViewController.swift
@@ -19,10 +19,10 @@ class MapViewController: UIViewController {
         setMapCoordinate()
     }
     
- 
-    
     func setMapCoordinate() {
         mapView.delegate = self
+        
+        // Pass all the details to the MapViewController
         for placeDetail in placeDetails {
             guard let coordinate = placeDetail.coordinate else { return }
             let annotation = MKPointAnnotation()
@@ -31,9 +31,9 @@ class MapViewController: UIViewController {
             mapView.addAnnotation(annotation)
             centerMapOnLocation(location: coordinate)
         }
+
         mapView.showsUserLocation = true
     }
-    
     
     func centerMapOnLocation(location: CLLocationCoordinate2D) {
         let radius = 5000
@@ -51,12 +51,12 @@ extension MapViewController: MKMapViewDelegate {
         
         if annotation.isKind(of: MKUserLocation.self) {
             return nil
-        }
+        }    
         
         let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "reusablePin")
         view.canShowCallout = true
         view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        view.pinTintColor = UIColor.red
+        view.pinTintColor = UIColor.red 
         
         return view
         
@@ -64,8 +64,8 @@ extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         let location = view.annotation
-        
         let launchingOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeTransit]
+        
         if let coordnate = location?.coordinate {
             location?.mapItem(coordinate: coordnate).openInMaps(launchOptions: launchingOptions)
         }
